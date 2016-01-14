b36f5fa65c90cd74e3a5c4a6268111246d1bb49c
Matt Turnure <mturnure@salesforce.com>
2016-01-14 17:12:10 -0500

    changed name of file

--- demo/app.js
+++ demo/app.js
@@ -303,8 +303,7 @@ negSuf:"",posPre:"\u00a4",posSuf:""}]},id:"en-us",pluralCat:function(a,c){var e=
     function HomeController($scope, viewport, $log, DataFactory) {
         var vm            = this,
             results       = doc.getElementById('viewport-type'),
-            body          = doc.body,
-            viewportState = '';
+            body          = doc.body;
 
         $scope.viewportType = viewport.getType();
         $scope.data         = [];
@@ -342,22 +341,19 @@ negSuf:"",posPre:"\u00a4",posSuf:""}]},id:"en-us",pluralCat:function(a,c){var e=
             if (viewport.getType() === 'handheld' || viewport.getType() === 'mini') {
                 DataFactory.getSmallData()
                 .then(function (response) {
-                    viewportState = viewport.getType();
-                    $scope.dataListSize = 'Less data is loaded for ' + viewportState;
+                    $scope.dataListSize = 'Less data is loaded for ' + viewport.getType();
                     $scope.data = response;
                 });
             }
 
             if (viewport.getType() === 'mini') {
-                viewportState = viewport.getType();
-                $scope.dataListSize = 'Less data is loaded for ' + viewportState;
+                $scope.dataListSize = 'Less data is loaded for ' + viewport.getType();
             }
 
             if (viewport.getType() === 'tablet') {
                 DataFactory.getMediumData()
                 .then(function (response) {
-                    viewportState = viewport.getType();
-                    $scope.dataListSize = 'More data is loaded for ' + viewportState;
+                    $scope.dataListSize = 'More data is loaded for ' + viewport.getType();
                     $scope.data = response;
                 });
             }
@@ -365,8 +361,7 @@ negSuf:"",posPre:"\u00a4",posSuf:""}]},id:"en-us",pluralCat:function(a,c){var e=
             if (viewport.getType() === 'widescreen') {
                 DataFactory.getBigData()
                 .then(function (response) {
-                    viewportState = viewport.getType();
-                    $scope.dataListSize = 'The most data is loaded for ' + viewportState;
+                    $scope.dataListSize = 'The most data is loaded for ' + viewport.getType();
                     $scope.data = response;
                 });
             }
